975cb54e43ca9e9beaf48446cf8c2a7b4fc13a31
Shikhar <shikhardadhich@gmail.com>
2023-08-23 17:15:23 +0530

    added node js server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path'); // Import the path module
+const bodyParser = require('body-parser');
+const app = express();
+const port =  80;
+
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+//app.use(cors()); // Enable CORS for all routes
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+    const filePath = path.join(__dirname, '/index.html');
+
+    // Use res.sendFile() to send the HTML file
+    res.sendFile(__dirname + "/index.html");
+  });
